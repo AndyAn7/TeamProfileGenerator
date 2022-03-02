de8583b8e2adebb80b48771640939e3114a07c84
Andrew An <andyan7@gmail.com>
2022-03-01 23:57:32 -0800

    Added properties to intern test object

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -5,3 +5,15 @@ test('Initializes intern object', () => {
 
     expect(intern.school).toEqual(expect.any(String));
 });
+
+test('Retrieves Intern education', () => {
+    const intern = new Intern('Andy', 22, 'andyan7@gmail.com', 'Univ. of WA');
+
+    expect(intern.getEd()).toEqual(expect.stringContaining(intern.school.toString()));
+});
+
+test('Retrieves role of Intern', () => {
+    const intern = new Intern('Andy', 22, 'andyan7@gmail.com', 'Univ. of WA');
+
+    expect(intern.getRole()).toEqual('Intern');
+});
