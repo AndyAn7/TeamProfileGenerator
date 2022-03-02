7d7667d8f6cb290e0e2c367b045858cf3705072f
Andrew An <andyan7@gmail.com>
2022-03-01 23:22:11 -0800

    Added employee properties

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -7,3 +7,27 @@ test ('initializes employee object', () => {
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
 });
+
+test ('retrieves employee name', () => {
+    const employee = new Employee ('Andy', 22, 'andyan7@gmail.com');
+
+    expect(employee.getName()).toEqual(expect.any(String));
+});
+
+test ('retrieves employee Id', () => {
+    const employee = new Employee ('Andy', 22, 'andyan7@gmail.com');
+
+    expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+test ('retrieves employee email', () => {
+    const employee = new Employee ('Andy', 22, 'andyan7@gmail.com');
+
+    expect(employee.getEmail()).toEqual(expect.any(String));
+});
+
+test ('retrieves employee role', () => {
+    const employee = new Employee ('Andy', 22, 'andyan7@gmail.com');
+
+    expect(employee.getRole()).toEqual('Employee');
+});
