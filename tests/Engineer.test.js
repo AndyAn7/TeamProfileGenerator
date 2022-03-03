657dd48cecde9a1127a488d3268dad2c6bac2887
Andrew An <andyan7@gmail.com>
2022-03-02 20:54:45 -0800

    corrected engineer.js module and acute syntactical changes

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,1 +1,9 @@
-const Engineer = require('../lib/Engineer')
+const Engineer = require('../lib/Engineer');
+
+test('initializes engineer object', () => {
+    const engineer = new Engineer('Andy', 22, 'andyan7@gmail.com', 'andyan7');
+
+    expect(engineer.github).toEqual(expect.any(String));
+});
+
+test('retrieves engineer')
